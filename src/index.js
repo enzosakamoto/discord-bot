fcf023c27a1471ee01313e8859ab711dfe5bcdbf
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-02-07 11:17:24 -0300

    add prettier file and new commands

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
-const { Client, IntentsBitField } = require("discord.js");
-require("dotenv").config();
+const { Client, IntentsBitField } = require('discord.js')
+require('dotenv').config()
 
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
-  ],
-});
+    IntentsBitField.Flags.MessageContent
+  ]
+})
 
-client.on("ready", (bot) => {
-  console.log(`✅ ${bot.user.tag} is ready`);
-});
+client.on('ready', (bot) => {
+  console.log(`✅ ${bot.user.tag} is ready`)
+})
 
-client.on("messageCreate", (message) => {
-  if (message.content === "ping") {
-    message.reply("pong");
+client.on('messageCreate', (message) => {
+  if (message.content === 'ping') {
+    message.reply('pong')
   }
 
-  if (message.content === "saka") {
-    message.reply("o mais lindo do mundo");
+  if (message.content === 'saka') {
+    message.reply('o mais lindo do mundo')
   }
-});
 
-client.login(process.env.TOKEN);
+  if (
+    message.content === 'naka' ||
+    message.content === 'recruta' ||
+    message.content === 'orto'
+  ) {
+    message.reply('maconha')
+  }
+})
+
+client.login(process.env.TOKEN)
